0408a2cd699a6592f861382500798d9f59d52644
a <a@hoge.com>
2025-03-13 12:10:59 +0900

    クエスト終了時の金額加算がされてなかった

--- Mod_MoneyFactor/src/MoneyFactor.cs
+++ Mod_MoneyFactor/src/MoneyFactor.cs
@@ -10,14 +10,33 @@ namespace Mod
 	[HarmonyLib.HarmonyPatch]
 	class MoneyFactor
 	{
+		// お金アイテムのIDらしい.
+		static readonly int c_MoneyItemID = "6b6e6f54-99b6-4f02-a460-4c020cd222a2".GetHashCode();
+
 		/// <summary>
-		/// キャラの経験値付与関数.
+		/// お金付与関数.
+		/// 派遣依頼の報酬などはこちらで処理されるようだ.
 		/// </summary>
-		/// <param name="__instance"></param>
 		[HarmonyLib.HarmonyPrefix]
 		[HarmonyLib.HarmonyPatch(typeof(HubManager), "AddGold")]
 		static void _Prefix_AddGold(ref int amount) {
-			amount = UnityEngine.Mathf.CeilToInt(amount * Plugin.Instance.ModConfig.Factor.Value);
+			int newAmount = UnityEngine.Mathf.CeilToInt(amount * Plugin.Instance.ModConfig.Factor.Value);
+		//	DebugUtil.LogError($"!!!!! {amount} -> {newAmount}");
+			amount = newAmount;
+		}
+
+		/// <summary>
+		/// お金付与関数.
+		/// クエスト終了時にもらえるものはこっちでアイテムとして処理されるらしい.
+		/// </summary>
+		[HarmonyLib.HarmonyPrefix]
+		[HarmonyLib.HarmonyPatch(typeof(Database), "AddGuildItem")]
+		static void _Prefix_AddGuildItem(BaseItem baseItem, ref int amount) {
+			if (baseItem.ID.GetHashCode() != c_MoneyItemID)
+				return;
+			int newAmount = UnityEngine.Mathf.CeilToInt(amount * Plugin.Instance.ModConfig.Factor.Value);
+		//	DebugUtil.LogError($"!!!!! {amount} -> {newAmount}");
+			amount = newAmount;
 		}
 
 
